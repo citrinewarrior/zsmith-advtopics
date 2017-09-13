f08c5d0bb2f90f377d004aa90991c0dea370e48b
Programming <programming@USProgramming5.local>
2017-09-13 13:00:48 -0400

    update

---
+++ Bank-account.py
@@ -0,0 +1,11 @@
+#Make a class BankAccount where instances of accounts can be created.
+#Give these objects variables such as account number, account owner, and account balance.
+#Create methods to deposit, withdraw, and check balance.
+class BankAccount:
+
+    def __init__():
+        self.number = accnumber
+        self.owner = owner
+        self.balance = balance
+
+acc1 = BankAccount(1337, "Noodle", 1)
