5b03bd9756274db81ba44f003f1d6990d20b67c6
Programming <programming@USProgramming5.local>
2017-09-15 12:59:34 -0400

    yay!

--- Bank-account.py
+++ Bank-account.py
@@ -2,10 +2,42 @@
 #Give these objects variables such as account number, account owner, and account balance.
 #Create methods to deposit, withdraw, and check balance.
 class BankAccount:
-
-    def __init__():
-        self.number = accnumber
+#SET BALANCE IN THE INIT TO 0
+    def __init__(self,number,owner):
+        self.number = number
         self.owner = owner
-        self.balance = balance
+        self.balance = 0
 
-acc1 = BankAccount(1337, "Noodle", 1)
+
+    def displayaccolder(self):
+        print "Account Number:", self.number
+        print "Account Holder:", self.owner
+        print "Current Balance:", self.balance
+
+    def deposit(self,amount):
+        self.balance = self.balance + amount
+
+    def withdraw(self,amount):
+        self.balance = self.balance - amount
+
+    def checkbalance(self):
+        print "Your current account balance is: ", self.balance, "dollars"
+
+#down here, are existing test accounts.
+acc1 = BankAccount(1337, "Noodle")
+acc2 = BankAccount(2005, "2D")
+acc3 = BankAccount(666, "Murdoc")
+acc4 = BankAccount(32, "Russel",)
+
+
+acc1.deposit(50)
+acc1.displayaccolder()
+acc1.withdraw(25)
+acc1.checkbalance()
+
+
+
+
+
+
+#down here goes the code to display the information
